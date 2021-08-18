40365783de677158738a4725ed96651a0afd914a
amirfakhrullah <amirfakhrullah96@gmail.com>
2021-08-19 00:51:41 +0800

    tutorial done

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import AddToList from "./components/AddToList";
+
+import List from './components/List';
+
+export interface IState {
+  people: {
+    name: string;
+    age: number;
+    url: string;
+    note?: string;
+  }[];
+}
 
 function App() {
+  const [people, setPeople] = useState<IState["people"]>([
+    {
+      name: "LeBron James",
+      url: "https://cdn.shopify.com/s/files/1/0414/8917/0599/collections/NBA_Store_Player_Headshot_Guide_-_LeBron_James.png?v=1592988861",
+      age: 36,
+      note: "Allergic to staying on the same team"
+    },
+    {
+      name: "Kobe Bryant",
+      url: "https://epmgaa.media.clients.ellingtoncms.com/img/photos/2020/02/27/Kobe_Bryant_headshot_smile.jpg",
+      age: 42,
+      note: "GOAT"
+    }
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>People Invited to My Party</h1>
+      <List people={people} />
+      <AddToList people={people} setPeople={setPeople} />
     </div>
   );
 }
